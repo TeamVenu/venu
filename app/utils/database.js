d992100725c339a39ec4a40488bf0bd0adcbfe1f
Erick <ericksaurisyl@gmail.com>
2017-03-29 15:24:59 -0400

    Added Database config data

--- app/utils/database.js
+++ app/utils/database.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
+  apiKey: 'AIzaSyB_dwG4qtan4dhvvpbjKG9rzkEbVPYklEs',
+  authDomain: 'venu-754b2.firebaseapp.com',
+  databaseURL: 'https://venu-754b2.firebaseio.com',
+  storageBucket: 'venu-754b2.appspot.com',
+  messagingSenderId: '131036382275',
 };
 
 firebase.initializeApp(config);
